62dec2b38bb2ecdc7598ee29bb3a103710cdbd3a
Richard Neumann <r.neumann@homeinfo.de>
2019-01-28 15:39:25 +0100

    Removed basicConfig call.

--- cmslib/config.py
+++ cmslib/config.py
@@ -1,6 +1,6 @@
 """DSCMS4 configuration."""
 
-from logging import INFO, basicConfig, getLogger
+from logging import getLogger
 
 from configlib import loadcfg
 
@@ -10,5 +10,4 @@ __all__ = ['CONFIG', 'LOG_FORMAT', 'LOGGER']
 
 CONFIG = loadcfg('cmslib.conf')
 LOG_FORMAT = '[%(levelname)s] %(name)s: %(message)s'
-basicConfig(level=INFO, format=LOG_FORMAT)
 LOGGER = getLogger('cmslib')
