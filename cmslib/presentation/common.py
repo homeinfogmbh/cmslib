9f31755da89e7fb62261e794c3017199a2337372
Richard Neumann <r.neumann@homeinfo.de>
2019-01-28 12:06:05 +0100

    Fixed MenuTreeItem.to_json() call.

--- cmslib/presentation/common.py
+++ cmslib/presentation/common.py
@@ -197,7 +197,6 @@ class PresentationMixin:
             'playlist': [
                 chart.to_json(mode=ChartMode.BRIEF, fk_fields=False)
                 for chart in self.playlist],
-            'menuItems': [
-                item.to_json(fk_fields=False) for item in self.menutree],
+            'menuItems': [item.to_json() for item in self.menutree],
             'charts': [
                 chart.to_json(fk_fields=False) for chart in self.charts]}
