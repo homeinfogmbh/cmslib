ba2d3617bff84367a88f668b0c002adc78808c2b
Richard Neumann <r.neumann@homeinfo.de>
2019-11-04 12:01:23 +0100

    Added BaseChartSchedule JSON methods.

--- cmslib/messages/schedule.py
+++ cmslib/messages/schedule.py
@@ -7,7 +7,9 @@ __all__ = [
     'NO_SUCH_SCHEDULE',
     'SCHEDULE_ADDED',
     'SCHEDULE_PATCHED',
-    'SCHEDULE_DELETED'
+    'SCHEDULE_DELETED',
+    'NO_BASE_CHART_SPECIFIED',
+    'NO_SCHEDULE_SPECIFIED'
 ]
 
 
@@ -16,3 +18,5 @@ NO_SUCH_SCHEDULE = JSONMessage(
 SCHEDULE_ADDED = JSONMessage('The schedule has been added.', status=201)
 SCHEDULE_PATCHED = JSONMessage('The schedule has been patched.', status=200)
 SCHEDULE_DELETED = JSONMessage('The schedule has been deleted.', status=200)
+NO_BASE_CHART_SPECIFIED = JSONMessage('No base chart specified.', status=400)
+NO_SCHEDULE_SPECIFIED = JSONMessage('No schedule specified.', status=400)
