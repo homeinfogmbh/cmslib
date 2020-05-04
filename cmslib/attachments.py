5229defd94f120281244051f84757652080a0316
Richard Neumann <mail@richard-neumann.de>
2020-05-04 11:26:06 +0200

    Refac.

--- cmslib/attachments.py
+++ cmslib/attachments.py
@@ -1,7 +1,5 @@
 """DOM utilities."""
 
-from hisfs import File
-
 from cmslib.dom import Attachment   # pylint: disable=E0401,E0611
 
 
@@ -9,48 +7,39 @@ __all__ = ['attachment_dom', 'attachment_json']
 
 
 # pylint: disable=W0622
-def attachment_dom(file_id, format=None, index=None):
+def attachment_dom(file, format=None, index=None):
     """Returns an attachment for the respective file ID."""
 
-    if file_id is None:
+    if file is None:
         return None
 
-    try:
-        file = File[file_id]
-    except File.DoesNotExist:
-        return None
-
+    metadata = file.metadata
     xml = Attachment()
     xml.id = file.id
-    xml.mimetype = file.metadata.mimetype
+    xml.mimetype = metadata.mimetype
     xml.filename = file.name
-    xml.sha256sum = file.metadata.sha256sum
-    xml.size = file.metadata.size
+    xml.sha256sum = metadata.sha256sum
+    xml.size = metadata.size
     xml.format = format
     xml.index = index
     return xml
 
 
-def attachment_json(file_id, json=None, format=None, index=None):
+def attachment_json(file, json=None, format=None, index=None):
     """Returns a JSON-ish representation of the attachment."""
 
-    if file_id is None:
+    if file is None:
         return json
 
-    try:
-        file = File[file_id]
-    except File.DoesNotExist:
-        return json
-
-    file_data = file.to_json(skip={'file'})
+    result = file.to_json(skip={'filedb_file'})
 
     if json:
-        file_data.update(json)
+        result.update(json)
 
     if format is not None:
-        file_data['format'] = format
+        result['format'] = format
 
     if index is not None:
-        file_data['index'] = index
+        result['index'] = index
 
-    return file_data
+    return result
