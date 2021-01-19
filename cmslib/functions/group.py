5ec452361689a91629d15e4916658ee5352e4f54
Richard Neumann <mail@richard-neumann.de>
2021-01-19 20:52:51 +0100

    Migrated to select_tree().

--- cmslib/functions/group.py
+++ cmslib/functions/group.py
@@ -1,6 +1,9 @@
 """Group related functions."""
 
+from peewee import ModelSelect
+
 from his import CUSTOMER
+from peeweeplus import select_tree
 
 from cmslib.messages.group import NO_SUCH_GROUP
 from cmslib.orm.group import Group
@@ -9,13 +12,16 @@ from cmslib.orm.group import Group
 __all__ = ['get_group']
 
 
+def list_group() -> ModelSelect:
+    """Selects the groups of the current customer."""
+
+    return select_tree(Group).where(Group.customer == CUSTOMER.id)
+
+
 def get_group(ident: int) -> Group:
     """Returns the respective group of the current customer."""
 
-    condition = Group.customer == CUSTOMER.id
-    condition &= Group.id == ident
-
     try:
-        return Group.get(condition)
+        return list_group().where(Group.id == ident).get()
     except Group.DoesNotExist:
         raise NO_SUCH_GROUP from None
