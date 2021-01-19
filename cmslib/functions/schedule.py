0078f7539c67b6b7c4f3f8a6f30746b1d364754d
Richard Neumann <mail@richard-neumann.de>
2021-01-19 21:47:35 +0100

    Migrated to explicit selects.

--- cmslib/functions/schedule.py
+++ cmslib/functions/schedule.py
@@ -1,6 +1,9 @@
 """Schedule related functions."""
 
+from peewee import ModelSelect
+
 from his import CUSTOMER
+from mdb import Address, Company, Customer
 
 from cmslib.messages.schedule import NO_SUCH_SCHEDULE
 from cmslib.orm.schedule import Schedule
@@ -9,13 +12,18 @@ from cmslib.orm.schedule import Schedule
 __all__ = ['get_schedule']
 
 
+def list_schedules() -> ModelSelect:
+    """Selects the schedules of the current customer."""
+
+    select = Schedule.select(Schedule, Customer, Company, Address)
+    select = select.join(Customer).join(Company).join(Address)
+    return select.where(Schedule.customer == CUSTOMER.id)
+
+
 def get_schedule(ident: int) -> Schedule:
     """Returns the respective schedule."""
 
-    condition = Schedule.id == ident
-    condition &= Schedule.customer == CUSTOMER.id
-
     try:
-        return Schedule.get(condition)
+        return list_schedules().get(Schedule.id == ident)
     except Schedule.DoesNotExist:
         raise NO_SUCH_SCHEDULE from None
