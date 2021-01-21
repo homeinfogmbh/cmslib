378ee8c9fe8cf90f98c823ae8797821a814c88b4
Richard Neumann <mail@richard-neumann.de>
2021-01-21 10:52:29 +0100

    Refac.

--- cmslib/functions/configuration.py
+++ cmslib/functions/configuration.py
@@ -3,9 +3,7 @@
 from peewee import ModelSelect
 
 from his import CUSTOMER
-from mdb import Address, Company, Customer
 
-from cmslib.messages.configuration import NO_SUCH_CONFIGURATION
 from cmslib.orm.configuration import Configuration
 
 
@@ -15,15 +13,11 @@ __all__ = ['get_configuration', 'list_configurations']
 def list_configurations() -> ModelSelect:
     """Returns the respective configuration."""
 
-    select = Configuration.select(Configuration, Customer, Company, Address)
-    select = select.join(Customer).join(Company).join(Address)
-    return select.where(Configuration.customer == CUSTOMER.id)
+    return Configuration.select(cascade=True).where(
+        Configuration.customer == CUSTOMER.id)
 
 
 def get_configuration(ident: int) -> Configuration:
     """Returns the respective configuration."""
 
-    try:
-        return list_configurations().where(Configuration.id == ident).get()
-    except Configuration.DoesNotExist:
-        raise NO_SUCH_CONFIGURATION from None
+    return list_configurations().where(Configuration.id == ident).get()
