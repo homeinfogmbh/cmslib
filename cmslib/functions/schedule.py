2e01acd5004e09c6b0eec425c4c561c0e03697a8
Richard Neumann <mail@richard-neumann.de>
2021-01-21 10:49:37 +0100

    Refac.

--- cmslib/functions/schedule.py
+++ cmslib/functions/schedule.py
@@ -3,27 +3,21 @@
 from peewee import ModelSelect
 
 from his import CUSTOMER
-from mdb import Address, Company, Customer
 
-from cmslib.messages.schedule import NO_SUCH_SCHEDULE
 from cmslib.orm.schedule import Schedule
 
 
-__all__ = ['get_schedule']
+__all__ = ['get_schedule', 'list_schedules']
 
 
 def list_schedules() -> ModelSelect:
     """Selects the schedules of the current customer."""
 
-    select = Schedule.select(Schedule, Customer, Company, Address)
-    select = select.join(Customer).join(Company).join(Address)
-    return select.where(Schedule.customer == CUSTOMER.id)
+    return Schedule.select(cascade=True).where(
+        Schedule.customer == CUSTOMER.id)
 
 
 def get_schedule(ident: int) -> Schedule:
     """Returns the respective schedule."""
 
-    try:
-        return list_schedules().get(Schedule.id == ident)
-    except Schedule.DoesNotExist:
-        raise NO_SUCH_SCHEDULE from None
+    return list_schedules().where(Schedule.id == ident).get()
