af35b72ca812c90a5f78875fd238d752bac09a90
Richard Neumann <mail@richard-neumann.de>
2021-01-21 13:50:16 +0100

    Renamed functions.

--- cmslib/functions/menu.py
+++ cmslib/functions/menu.py
@@ -9,10 +9,17 @@ from his import CUSTOMER
 from cmslib.orm.menu import Menu, MenuItem, MenuItemChart
 
 
-__all__ = ['get_menu', 'get_menu_item', 'get_menu_item_chart']
+__all__ = [
+    'get_menu',
+    'get_menus',
+    'get_menu_item',
+    'get_menu_items',
+    'get_menu_item_chart',
+    'get_menu_item_charts'
+]
 
 
-def list_menus() -> ModelSelect:
+def get_menus() -> ModelSelect:
     """Lists the menus of the current customer."""
 
     return Menu.select(cascade=True).where(Menu.customer == CUSTOMER.id)
@@ -21,10 +28,10 @@ def list_menus() -> ModelSelect:
 def get_menu(ident: int) -> Menu:
     """Returns the respective menu of the current customer."""
 
-    return list_menus().where(Menu.id == ident).get()
+    return get_menus().where(Menu.id == ident).get()
 
 
-def list_menu_items(menu: Optional[Union[Menu, int]] = None) -> ModelSelect:
+def get_menu_items(menu: Optional[Union[Menu, int]] = None) -> ModelSelect:
     """Lists the menu items of the current customer."""
 
     condition = Menu.customer == CUSTOMER.id
@@ -38,10 +45,10 @@ def list_menu_items(menu: Optional[Union[Menu, int]] = None) -> ModelSelect:
 def get_menu_item(ident: int) -> MenuItem:
     """Returns the respective menu item."""
 
-    return list_menu_items().where(MenuItem.id == ident).get()
+    return get_menu_items().where(MenuItem.id == ident).get()
 
 
-def list_menu_item_charts(
+def get_menu_item_charts(
         menu: Optional[Union[Menu, int]] = None) -> ModelSelect:
     """Selects the menu item charts of the current customer."""
 
@@ -56,4 +63,4 @@ def list_menu_item_charts(
 def get_menu_item_chart(ident: int) -> MenuItemChart:
     """Returns the respective MenuItemChart."""
 
-    return list_menu_item_charts().where(MenuItemChart.id == ident).get()
+    return get_menu_item_charts().where(MenuItemChart.id == ident).get()
