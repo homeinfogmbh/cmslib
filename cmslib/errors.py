e057b470f03f42f1709e764268fcf59492f457a1
Richard Neumann <mail@richard-neumann.de>
2021-01-21 10:18:28 +0100

    Added exceptions and error handlers.

---
+++ cmslib/errors.py
@@ -0,0 +1,30 @@
+"""Error messages and mappings."""
+
+from wsgilib import JSONMessage
+
+from cmslib.exceptions import AmbiguousBaseChart
+from cmslib.exceptions import AmbiguousConfigurationsError
+from cmslib.exceptions import CircularReference
+from cmslib.exceptions import NoConfigurationFound
+from cmslib.exceptions import OrphanedBaseChart
+
+
+__all__ = ['ERRORS']
+
+
+ERRORS = {
+    AmbiguousBaseChart: lambda error: JSONMessage(
+        'Ambiguous base chart.', base_chart=error.base_chart.to_json(),
+        references=[ref.to_json() for ref in error.references], status=400),
+    AmbiguousConfigurationsError: lambda error: JSONMessage(
+        'Ambiguous configurations.', level=error.level, index=error.index,
+        status=400),
+    CircularReference: lambda error: JSONMessage(
+        'Circular reference.', type=type(error.model).__name__,
+        model=error.model.to_json(), status=400),
+    NoConfigurationFound: lambda _: JSONMessage(
+        'No configuration found.', status=404),
+    OrphanedBaseChart: lambda error: JSONMessage(
+        'Orphaned base chart.', base_chart=error.base_chart.to_json(),
+        status=400),
+}
--- cmslib/exceptions.py
+++ cmslib/exceptions.py
@@ -6,24 +6,12 @@ from peewee import Model
 
 
 __all__ = [
-    'OrphanedBaseChart',
     'AmbiguousBaseChart',
     'AmbiguousConfigurationsError',
-    'NoConfigurationFound'
+    'CircularReference',
+    'NoConfigurationFound',
+    'OrphanedBaseChart',
 ]
-
-
-class OrphanedBaseChart(Exception):
-    """Indicates that the respective base chart is orphaned."""
-
-    def __init__(self, base_chart: Model):
-        """Sets the base chart."""
-        super().__init__(base_chart)
-        self.base_chart = base_chart
-
-    def __str__(self):
-        """Returns an appropriate message."""
-        return f'Base chart {self.base_chart.id} is orphaned.'
 
 
 class AmbiguousBaseChart(Exception):
@@ -32,7 +20,6 @@ class AmbiguousBaseChart(Exception):
     """
 
     def __init__(self, base_chart: Model, references: Iterable[Model]):
-        """Sets the respective base chart and referencing charts."""
         super().__init__(base_chart, references)
         self.base_chart = base_chart
         self.references = references
@@ -47,11 +34,39 @@ class AmbiguousConfigurationsError(Exception):
     """Indicates that ambiguous configurations are defined for an object."""
 
     def __init__(self, level: int, index: int):
-        """Sets the level and its index."""
         super().__init__(level, index)
         self.level = level
         self.index = index
 
 
+class CircularReference(Exception):
+    """Indicates a circular reference within a tree structure."""
+
+    def __init__(self, model: Model):
+        super().__init__(model)
+        self.model = model
+
+
+class DifferentMenus(Exception):
+    """Indicates that menu items belong to different menus."""
+
+    def __init__(self, menu: Model, other: Model):
+        super().__init__(menu, other)
+        self.menu = menu
+        self.other = other
+
+
 class NoConfigurationFound(Exception):
     """Indicates that no configuration has been found."""
+
+
+class OrphanedBaseChart(Exception):
+    """Indicates that the respective base chart is orphaned."""
+
+    def __init__(self, base_chart: Model):
+        super().__init__(base_chart)
+        self.base_chart = base_chart
+
+    def __str__(self):
+        """Returns an appropriate message."""
+        return f'Base chart {self.base_chart.id} is orphaned.'
