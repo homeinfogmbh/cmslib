90e7e373c151ff93afabb3787c4a634530495e86
Richard Neumann <mail@richard-neumann.de>
2021-01-21 11:17:36 +0100

    Added select(cascade=False) to deployment content models.

--- cmslib/orm/content/deployment.py
+++ cmslib/orm/content/deployment.py
@@ -2,9 +2,10 @@
 
 from __future__ import annotations
 
-from peewee import ForeignKeyField, IntegerField
+from peewee import JOIN, ForeignKeyField, IntegerField, ModelSelect
 
 from hwdb import Deployment
+from mdb import Address, Company, Customer
 
 from cmslib.orm.charts import BaseChart, Chart, ChartMode
 from cmslib.orm.common import DSCMS4Model
@@ -25,6 +26,27 @@ class DeploymentContent(DSCMS4Model):
     deplyoment = ForeignKeyField(
         Deployment, column_name='deployment', on_delete='CASCADE')
 
+    @classmethod
+    def select(cls, *args, cascade: bool = False, **kwargs) -> ModelSelect:
+        """Selects records."""
+        if not cascade:
+            return super().select(*args, **kwargs)
+
+        deployment_address = Address.alias()
+        lpt_address = Address.alias()
+        args = {
+            cls, Deployment, Customer, Company, Address,
+            deployment_address, lpt_address, *args
+        }
+        return super().select(*args, **kwargs).join(
+            Deployment).join(Customer).join(Company).join(Address).join_from(
+            Deployment, deployment_address,
+            on=Deployment.address == deployment_address.id).join_from(
+            Deployment, lpt_address,
+            on=Deployment.lpt_address == lpt_address.id,
+            join_type=JOIN.LEFT_OUTER
+        )
+
 
 class DeploymentBaseChart(DeploymentContent):
     """Association of a base chart with a Deployment."""
@@ -44,6 +66,23 @@ class DeploymentBaseChart(DeploymentContent):
         record.deployment = deployment
         record.base_chart = base_chart
         return record
+
+    @classmethod
+    def select(cls, *args, cascade: bool = False, **kwargs) -> ModelSelect:
+        """Selects records."""
+        if not cascade:
+            return super().select(*args, **kwargs)
+
+        base_chart_customer = Customer.alias()
+        base_chart_company = Company.alias()
+        base_chart_address = Address.alias()
+        args = {
+            cls, BaseChart, base_chart_customer, base_chart_company,
+            base_chart_address
+        }
+        return super().select(*args, **kwargs).join(
+            BaseChart).join(base_chart_customer).join(base_chart_company).join(
+            base_chart_address)
 
     @property
     def chart(self) -> Chart:
@@ -68,6 +107,23 @@ class DeploymentConfiguration(DeploymentContent):
     configuration = ForeignKeyField(
         Configuration, column_name='configuration', on_delete='CASCADE')
 
+    @classmethod
+    def select(cls, *args, cascade: bool = False, **kwargs) -> ModelSelect:
+        """Selects records."""
+        if not cascade:
+            return super().select(*args, **kwargs)
+
+        configuration_customer = Customer.alias()
+        configuration_company = Company.alias()
+        configuration_address = Address.alias()
+        args = {
+            cls, Configuration, configuration_customer, configuration_company,
+            configuration_address
+        }
+        return super().select(*args, **kwargs).join(
+            Configuration).join(configuration_customer).join(
+            configuration_company).join(configuration_address)
+
     def to_json(self) -> dict:
         """Returns a JSON-ish dict."""
         return {'id': self.id, 'configuration': self.configuration_id}
@@ -80,6 +136,19 @@ class DeploymentMenu(DeploymentContent):
         table_name = 'deployment_menu'
 
     menu = ForeignKeyField(Menu, column_name='menu', on_delete='CASCADE')
+
+    @classmethod
+    def select(cls, *args, cascade: bool = False, **kwargs) -> ModelSelect:
+        """Selects records."""
+        if not cascade:
+            return super().select(*args, **kwargs)
+
+        menu_customer = Customer.alias()
+        menu_company = Company.alias()
+        menu_address = Address.alias()
+        args = {cls, Menu, menu_customer, menu_company, menu_address}
+        return super().select(*args, **kwargs).join(
+            Menu).join(menu_customer).join(menu_company).join(menu_address)
 
     def to_json(self) -> dict:
         """Returns a JSON-ish dict."""
