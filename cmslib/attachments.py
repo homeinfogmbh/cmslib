fdf9c59de9b9d130c4f47b5439c68bfdbe8d3821
Richard Neumann <mail@richard-neumann.de>
2021-01-21 09:51:53 +0100

    Refactored exceptions.

--- cmslib/attachments.py
+++ cmslib/attachments.py
@@ -1,5 +1,8 @@
 """DOM utilities."""
 
+from typing import Union
+
+from filedb import File as FileDBFile
 from hisfs import File
 
 from cmslib.dom import Attachment   # pylint: disable=E0401,E0611
@@ -9,20 +12,23 @@ __all__ = ['attachment_dom', 'attachment_json']
 
 
 # pylint: disable=W0622
-def attachment_dom(file: File, format: str = None,
+def attachment_dom(file: Union[File, int, None], format: str = None,
                    index: int = None) -> Attachment:
     """Returns an attachment for the respective file ID."""
 
     if file is None:
         return None
 
-    metadata = file.metadata
+    if isinstance(file, int):
+        file = File.select(File, FileDBFile).join(FileDBFile).where(
+            File.id == file)
+
     xml = Attachment()
     xml.id = file.id
-    xml.mimetype = metadata.mimetype
+    xml.mimetype = file.mimetype
     xml.filename = file.name
-    xml.sha256sum = metadata.sha256sum
-    xml.size = metadata.size
+    xml.sha256sum = file.sha256sum
+    xml.size = file.size
     xml.format = format
     xml.index = index
     return xml
--- cmslib/exceptions.py
+++ cmslib/exceptions.py
@@ -1,5 +1,9 @@
 """Common ORM model exceptions."""
 
+from typing import Iterable
+
+from peewee import Model
+
 
 __all__ = [
     'OrphanedBaseChart',
@@ -9,14 +13,10 @@ __all__ = [
 ]
 
 
-class CMSLibError(Exception):
-    """Base class for exceptions within the DSCMS4."""
-
-
-class OrphanedBaseChart(CMSLibError):
+class OrphanedBaseChart(Exception):
     """Indicates that the respective base chart is orphaned."""
 
-    def __init__(self, base_chart):
+    def __init__(self, base_chart: Model):
         """Sets the base chart."""
         super().__init__(base_chart)
         self.base_chart = base_chart
@@ -26,16 +26,12 @@ class OrphanedBaseChart(CMSLibError):
         return f'Base chart {self.base_chart.id} is orphaned.'
 
 
-class AmbiguityError(CMSLibError):
-    """Indicates an error due to ambiguous information."""
-
-
-class AmbiguousBaseChart(AmbiguityError):
+class AmbiguousBaseChart(Exception):
     """Indicates that the respective base chart
     is referenced by more than one chart.
     """
 
-    def __init__(self, base_chart, references):
+    def __init__(self, base_chart: Model, references: Iterable[Model]):
         """Sets the respective base chart and referencing charts."""
         super().__init__(base_chart, references)
         self.base_chart = base_chart
@@ -47,15 +43,15 @@ class AmbiguousBaseChart(AmbiguityError):
         return f'Base chart #{self.base_chart.id} is ambiguous: {charts}.'
 
 
-class AmbiguousConfigurationsError(AmbiguityError):
+class AmbiguousConfigurationsError(Exception):
     """Indicates that ambiguous configurations are defined for an object."""
 
-    def __init__(self, level, index):
+    def __init__(self, level: int, index: int):
         """Sets the level and its index."""
         super().__init__(level, index)
         self.level = level
         self.index = index
 
 
-class NoConfigurationFound(CMSLibError):
+class NoConfigurationFound(Exception):
     """Indicates that no configuration has been found."""
