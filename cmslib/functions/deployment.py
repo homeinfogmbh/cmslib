f577596a401d54939c3ea33b5c7c22828555c992
Richard Neumann <mail@richard-neumann.de>
2021-05-14 12:16:19 +0200

    Add optional content joining to get_deployments()

--- cmslib/functions/deployment.py
+++ cmslib/functions/deployment.py
@@ -3,11 +3,17 @@
 from sys import stdout
 from typing import Callable
 
-from peewee import ModelSelect
+from peewee import JOIN, ModelSelect
 
 from functoolsplus import timeit
 from his import CUSTOMER
 from hwdb import Deployment
+
+from cmslib.functions.charts import get_trashed
+from cmslib.orm.charts import BaseChart
+from cmslib.orm.content.deployment import DeploymentBaseChart
+from cmslib.orm.content.deployment import DeploymentConfiguration
+from cmslib.orm.content.deployment import DeploymentMenu
 
 
 __all__ = ['get_deployment', 'get_deployments', 'with_deployment']
@@ -20,11 +26,23 @@ def get_deployment(ident: int) -> Deployment:
 
 
 @timeit(file=stdout, flush=True)
-def get_deployments() -> ModelSelect:
+def get_deployments(content: bool = False) -> ModelSelect:
     """Selects the deployments of the current customer."""
 
-    return Deployment.select(cascade=True).where(
-        Deployment.customer == CUSTOMER.id)
+    condition = Deployment.customer == CUSTOMER.id
+    select = Deployment.select(cascade=True)
+
+    if not content:
+        return select.where(condition)
+
+    condition &= get_trashed()
+    return select.join(
+        DeploymentBaseChart, join_type=JOIN.LEFT_OUTER).join(
+        BaseChart).join_from(
+        Deployment, DeploymentConfiguration,
+        join_type=JOIN.LEFT_OUTER).join_from(
+        Deployment, DeploymentMenu, join_type=JOIN.LEFT_OUTER
+    ).where(condition)
 
 
 def with_deployment(function: Callable) -> Callable:
