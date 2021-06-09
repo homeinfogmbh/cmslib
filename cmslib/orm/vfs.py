6f8a8b494f88ecaf7c07bba5bc2cede1ce7b2e1f
Richard Neumann <mail@richard-neumann.de>
2021-06-09 12:57:07 +0200

    Fix charts → base_charts renaming

--- cmslib/orm/vfs.py
+++ cmslib/orm/vfs.py
@@ -41,7 +41,7 @@ class Directory(TreeNode):
     def add_base_chart(self, base_chart: BaseChart):
         """Adds a base chart to the directory."""
         try:
-            return self.charts.where(
+            return self.base_charts.where(
                 ContentChart.base_chart == base_chart).get()
         except ContentChart.DoesNotExist:
             content_chart = ContentChart(directory=self, base_chart=base_chart)
@@ -50,7 +50,7 @@ class Directory(TreeNode):
 
     def remove_base_chart(self, base_chart: BaseChart):
         """Removes a base chart from the directory."""
-        for content_chart in self.charts.where(
+        for content_chart in self.base_charts.where(
                 ContentChart.base_chart == base_chart):
             content_chart.delete_instance()
 
@@ -67,8 +67,8 @@ class Directory(TreeNode):
         """Returns JSON-ish dict."""
         json = super().to_json(*args, **kwargs)
 
-        if charts:
-            json['baseCharts'] = [cc.base_chart_id for cc in self.charts]
+        if base_charts:
+            json['baseCharts'] = [cc.base_chart_id for cc in self.base_charts]
 
         return json
 
@@ -80,7 +80,7 @@ class ContentChart(DSCMS4Model):
         table_name = 'content_chart'
 
     directory = ForeignKeyField(
-        Directory, column_name='directory', backref='charts',
+        Directory, column_name='directory', backref='base_charts',
         on_delete='CASCADE', lazy_load=False)
     base_chart = ForeignKeyField(
         BaseChart, column_name='base_chart', on_delete='CASCADE',
