00c73749dbca18e73925f7372090cc7214d29da5
Richard Neumann <mail@richard-neumann.de>
2021-06-09 12:43:07 +0200

    Add get_root_base_charts()

--- cmslib/functions/vfs.py
+++ cmslib/functions/vfs.py
@@ -4,10 +4,15 @@ from peewee import ModelSelect
 
 from his import CUSTOMER
 
-from cmslib.orm.vfs import Directory
+from cmslib.orm.vfs import ContentChart, Directory
 
 
-__all__ = ['get_directories', 'get_root', 'get_directory']
+__all__ = [
+    'get_directories',
+    'get_root',
+    'get_directory',
+    'get_root_base_charts'
+]
 
 
 def get_directories() -> ModelSelect:
@@ -27,3 +32,13 @@ def get_directory(ident: int) -> Directory:
     """Selects a specific directory."""
 
     return get_directories().where(Directory.id == ident).get()
+
+
+def get_root_base_charts() -> ModelSelect:
+    """Lists root directories of the current customer."""
+
+    return BaseChart.select(cascase=True).join_from(
+        BaseChart, ContentChart, join_type=JOIN.LEFT_OUTER).where(
+        (BaseChart.customer == CUSTOMER.id)
+        & (ContentChart.directory >> None)
+    )
