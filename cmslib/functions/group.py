408e2bcfe50bfc3daf6948ff990894defa8db2d5
Richard Neumann <r.neumann@homeinfo.de>
2022-01-14 12:24:37 +0100

    Refactor

--- cmslib/functions/group.py
+++ cmslib/functions/group.py
@@ -14,51 +14,8 @@ __all__ = [
     'get_groups',
     'get_group_member_deployment',
     'get_group_member_deployments',
-    'get_tree'
+    'get_lineage'
 ]
-
-
-def get_parent(group: Group, groups: dict[int, Group]) -> Optional[Group]:
-    """Returns the parent of the group from the given groups."""
-
-    if group.parent_id is None:
-        return None
-
-    return groups.get(group.parent_id)
-
-
-def get_lineage(
-        group: Union[Group, int], *,
-        groups: Optional[dict[int, Group]] = None,
-        include_group: bool = True
-) -> Iterator[Group]:
-    """Returns the given group and all of
-    its parents throughout its lineage.
-    """
-
-    if isinstance(group, int):
-        group = Group[group]
-
-    if groups is None:
-        groups = {
-            group.id: group for group in
-            Group.select().where(Group.customer == group.customer)
-        }
-
-    if include_group:
-        yield group
-
-    while group := get_parent(group.parent_id, groups):
-        yield group
-
-
-def get_children(groups: Iterable[Group], parent: Group) -> list[Group]:
-    """Returns the children of the group."""
-
-    return sorted(
-        filter(lambda group: group.parent == parent, groups),
-        key=lambda group: group.index
-    )
 
 
 def get_group(ident: int) -> Group:
@@ -93,21 +50,26 @@ def get_group_member_deployments(
     return GroupMemberDeployment.select(cascade=True).where(condition)
 
 
-def get_condition(ident: Optional[int]) -> Callable[[Group], bool]:
-    """Returns a group filtering condition for get_tree()"""
+def get_lineage(
+        group: Union[Group, int], *,
+        groups: Optional[dict[int, Group]] = None,
+        include_group: bool = True
+) -> Iterator[Group]:
+    """Returns the given group and all of
+    its parents throughout its lineage.
+    """
 
-    if ident is None:
-        return lambda group: group.parent is None
+    if not isinstance(group, Group):
+        group = Group[group]
 
-    return lambda group: group.id == ident
+    if groups is None:
+        groups = {
+            group.id: group for group in
+            Group.select().where(Group.customer == group.customer)
+        }
 
+    if include_group:
+        yield group
 
-def get_tree(
-        groups: Iterable[Group], ident: Optional[int] = None
-) -> dict:
-    """Returns a dict of groups representing the groups tree."""
-
-    return {
-        root: get_children(groups, root)
-        for root in filter(get_condition(ident), groups)
-    }
+    while group := groups.get(group.parent_id):
+        yield group
